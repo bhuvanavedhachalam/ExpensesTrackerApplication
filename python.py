cd4e14a89473fe694ba322e08d57340566bebb81
bhuvanavedhachalam <bhuvanavedhachalam@gmail.com>
2026-05-27 09:55:51 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,39 @@
 
+expenses = []
+
+while True:
+    print("\n1.Add Expense")
+    print("2.View Expenses")
+    print("3.Total Expense")
+    print("4.Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        name = input("Enter expense name: ")
+        amount = float(input("Enter amount: "))
+
+        expenses.append([name, amount])
+
+        print("Expense Added")
+
+    elif choice == "2":
+        print("\nExpenses List")
+
+        for expense in expenses:
+            print(expense[0], "-", expense[1])
+
+    elif choice == "3":
+        total = 0
+
+        for expense in expenses:
+            total += expense[1]
+
+        print("Total Expense =", total)
+
+    elif choice == "4":
+        print("Thank You")
+        break
+
+    else:
+        print("Invalid Choice")
